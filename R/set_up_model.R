9c3c5dd24f775e1baa60891d26c9d1f1e02e1bf0
JDMunday <james.munday@lshtm.ac.uk>
2022-02-04 17:38:59 +0000

    added age dists

--- R/set_up_model.R
+++ R/set_up_model.R
@@ -4,12 +4,13 @@ library(rstanarm)
 library(ggplot2)
 
 # load samples of infections and abprev from inc2prev
-samples = readRDS('../../../inc2prev/outputs/samples_combined_age_ab.rds')
+samples = readRDS('../../../inc2prev/outputs/samples_combined_age_ab_A.rds')
 samples = data.table(samples)
 
 # load mean contact matrices from contactmatrixcode
 cms = readRDS('cms.rds')
 sr_dates = readRDS('sr_dates.rds')
+population = readRDS('population.rds')
 
 # create matrices of mean and standard deviations for infections and ab_prevalence
 inf_matrix_mean = dcast(samples[name=='infections', c('sample', 'variable', 'value', 'date')], 
@@ -38,19 +39,36 @@ setcolorder(inf_matrix_sd, colorder)
 setcolorder(anb_matrix_mean, colorder)
 setcolorder(anb_matrix_sd, colorder)
 
+anb_matrix_mean[, c('2-10', '11-15', '16-24', '25-34', '35-49', '50-69', '70+' )] = 
+  anb_matrix_mean[, c('2-10', '11-15', '16-24', '25-34', '35-49', '50-69', '70+' )] * 0.9 
+
+inf_matrix_mean[, c('2-10', '11-15', '16-24', '25-34', '35-49', '50-69', '70+' )] = 
+  data.table(t(t(inf_matrix_mean[, c('2-10', '11-15', '16-24', '25-34', '35-49', '50-69', '70+' )]) * 
+      tail(population$pop_total,-1)))
+
+inf_matrix_sd[, c('2-10', '11-15', '16-24', '25-34', '35-49', '50-69', '70+' )] = 
+  data.table(t(t(inf_matrix_sd[, c('2-10', '11-15', '16-24', '25-34', '35-49', '50-69', '70+' )]) * 
+      tail(population$pop_total,-1)))
+
+
+
+inf_matrix_mean[, sr := cut(date, breaks=c(sr_dates$min_date, max(sr_dates$max_date) + 100), labels = sr_dates$survey_round )]
+
+start_date = lubridate::ymd('20210401')
+end_date = lubridate::ymd('20211001')
 # filter matrices to correct dates
-inf_matrix_mean = inf_matrix_mean[date >= min(sr_dates$min_date)]
-inf_matrix_sd = inf_matrix_sd[date >= min(sr_dates$min_date)]
-anb_matrix_mean = anb_matrix_mean[date >= min(sr_dates$min_date)]
-anb_matrix_sd = anb_matrix_sd[date >= min(sr_dates$min_date)]
+inf_matrix_mean = inf_matrix_mean[date < end_date & date > start_date]
+inf_matrix_sd = inf_matrix_sd[date < end_date & date > start_date]
+anb_matrix_mean = anb_matrix_mean[date < end_date & date > start_date]
+anb_matrix_sd = anb_matrix_sd[date < end_date & date > start_date]
+sr_dates = sr_dates[min_date < end_date & max_date > start_date]
 
 # create vector to indicate correct contact matrix
-inf_matrix_mean[, sr := cut(date, breaks=c(sr_dates$min_date, max(sr_dates$max_date) + 100), labels = sr_dates$survey_round )]
 day_to_week = as.numeric(inf_matrix_mean$sr)
 day_to_week = day_to_week - min(day_to_week) + 1
 
 # load contact matrices into list
-contact_matrices = lapply(X=unique(sr_dates$survey_round), function(X){matrix(cms[sr==X]$cms, nrow=7)})
+contact_matrices = lapply(X=unique(sr_dates$survey_round), function(X){t(matrix(cms[sr==X]$cms, nrow=7))})
 
 # set up data input for stan 
 data = list( 
@@ -74,6 +92,9 @@ fit = sampling(age_mod, data, warmup=100, iter=500, chains=1, control = list(max
 pairs(fit, pars = c('inf_rate[1]', 'inf_rate[2]', 'inf_rate[3]', 'inf_rate[4]', 'inf_rate[5]', 'inf_rate[6]', 'inf_rate[7]'))
 pairs(fit, pars = c('susceptibility[1]', 'susceptibility[2]', 'susceptibility[3]', 'susceptibility[4]', 'susceptibility[5]', 'susceptibility[6]', 'susceptibility[7]'))
 
+stan_plot(fit, pars = c('inf_rate[1]', 'inf_rate[2]', 'inf_rate[3]', 'inf_rate[4]', 'inf_rate[5]', 'inf_rate[6]', 'inf_rate[7]'))
+stan_plot(fit, pars = c('susceptibility[1]', 'susceptibility[2]', 'susceptibility[3]', 'susceptibility[4]', 'susceptibility[5]', 'susceptibility[6]', 'susceptibility[7]'))
+
 
 fitmat = extract(fit)
 
@@ -96,21 +117,10 @@ pred_infs = rbindlist(lapply(1:7, pull_and_mod_inf, df=predicted_infections))
 samped_infs = rbindlist(lapply(1:7, pull_and_mod_inf, df=samples_infections))
 
 ggplot() + 
+  geom_point(data=samped_infs, aes(x=date, y=infections, group=date), alpha=0.002, color='black')+
   geom_point(data=pred_infs, aes(x=date, y=infections, color=age_group), alpha=0.002)+
-  geom_boxplot(data=samped_infs, aes(x=date, y=infections, color=age_group, group=date), alpha=0.2)+
   facet_wrap(~age_group)
 
 
-ggplot() + 
-  geom_point(data=samped_infs, aes(x=date, y=infections, color=age_group), alpha=0.002)+
-  geom_line(data=inf_matrix_mean, aes(x=date, y=`2-10`))
-  
-
-predicted_infections = data.table(matrix(predicted_infections, ncol=92))
-colnames(predicted_infections) = as.character(inf_matrix_mean$date)
-
-
-dim(fitmat$transmissibility_correction)
-
-
+saveRDS(fit, 'alpha_fit.rds')
 
--- stan/age_specific_transmission.stan
+++ stan/age_specific_transmission.stan
@@ -20,8 +20,8 @@ transformed data{
 parameters {
   // incidence of infection and ab prevalence modelled as parameters drawn from 
   // independent normal distributions per time step.
-  real <lower=0,upper=1> infections[T,A]; 
-  real <lower=0,upper=1> antibodies[T,A];
+  real <lower=0> infections[T,A]; 
+  real <lower=0, upper=1> antibodies[T,A];
   
   //parameters to fit
   real<lower=0,upper=1> inf_rate[A];       // age specific rate of infection conditional on contact with 100% susceptibility
@@ -49,7 +49,7 @@ model {
   // sample priors for infections and antibodies 
   for(t in 1:T){
     for(a in 1:A){
-      infections[t,a] ~ normal(inf_mu[t,a], inf_sd[t,a])T[0,1];
+      infections[t,a] ~ normal(inf_mu[t,a], inf_sd[t,a])T[0,];
       antibodies[t,a] ~ normal(anb_mu[t,a], anb_sd[t,a])T[0,1]; 
       
     }
@@ -68,12 +68,12 @@ model {
     transmissibility_correction = full_susceptibility * to_row_vector(inf_rate);
     
     // estimate next generation of infections
-    next_gen = (contact_matrices[day_to_week_converter[t]] .* transmissibility_correction)  * to_vector(infections_fixed[t,:]);
+    next_gen = (diag_matrix(full_susceptibility) * contact_matrices[day_to_week_converter[t]] * diag_matrix(to_vector(inf_rate)))  * to_vector(infections_fixed[t,:]);
     
     // fit model 
     for(a in 1:A){
-      infections_fixed[t+3, a] ~ normal(next_gen[a], inf_sd[t,a]);
-      //next_gen[a] ~ normal(inf_mu[t+3,a], inf_sd[t+3, a]);
+      //infections_fixed[t+3, a] ~ normal(next_gen[a], inf_sd[t,a]);
+      next_gen[a] ~ normal(inf_mu[t+3,a], inf_sd[t+3, a]);
     }
     
   }
@@ -90,13 +90,14 @@ generated quantities{
   
   for(t in 4:T){
     // combine inherent and ab susceptibility
-    full_susceptibility[t-3] = to_vector(susceptibility) .* (1.0 - (to_vector(antibodies[t-3,:])));
+    full_susceptibility[t-3] = to_vector(susceptibility) .* (1.0 - (to_vector(antibodies[t-3,:]) * ab_protection));
     
     // construct matrix to correct contact matrix to NGM
-    transmissibility_correction[t-3] = full_susceptibility[t-3] * to_row_vector(inf_rate);
+    transmissibility_correction[t-3] = full_susceptibility[t-3] * rep_row_vector(1,7);//to_row_vector(inf_rate);
     
     // estimate next generation of infections
-    next_gens[t] = (contact_matrices[day_to_week_converter[t-3]] .* transmissibility_correction[t-3])  * to_vector(infections_fixed[t-3,:]);
+    next_gens[t] = (diag_matrix(full_susceptibility[t-3]) * contact_matrices[day_to_week_converter[t-3]] * diag_matrix(to_vector(inf_rate)))  * to_vector(infections_fixed[t-3,:]);
+    //next_gens[t] = (contact_matrices[day_to_week_converter[t-3]] .* transmissibility_correction[t-3])  * to_vector(infections_fixed[t-3,:]);
   }
   
 }
