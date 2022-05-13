a36df7d00a52e82d590f64a6e042d4067341f6e3
JDMunday <james.munday@lshtm.ac.uk>
2022-05-13 14:17:55 +0100

    fix gamma param fit

--- R/run_one_model_once.R
+++ R/run_one_model_once.R
@@ -11,6 +11,7 @@ fit = fit_NGM_model_for_date_range(
   quantiles=c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95), 
   contact_option = 1
 )
+
 summary_preds = data.table(fit$summary_preds)
 summary_preds[, age_group := age_groups[age_index]]
 summary_preds[, date := forecast_date - 49 + time_index]
