a85a0bf2bf73af500a826239b3462e339b03f747
JDMunday <james.munday@lshtm.ac.uk>
2022-05-24 09:25:54 +0100

    adapt for case data

--- R/prep_and_run_cases_example.R
+++ R/prep_and_run_cases_example.R
@@ -36,7 +36,8 @@ case_matrix_mean[, date := lubridate::ymd(date)]
 cms_cases = readRDS('cms_cases.rds')
 
 age_mod_cases = cmdstan_model('stan/multi-option-contact-model-cases.stan')
-
+period = 15 
+smax = 15
 
 fit = fit_NGM_model_for_date_range_cases(
   end_date = dates[[6]],
@@ -52,20 +53,19 @@ fit = fit_NGM_model_for_date_range_cases(
   runindex = 1, 
   quantiles=c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95), 
   contact_option = 1,
-  sigma_option = 1
+  sigma_option = 1, 
+  contact_delay = 5
 )
 
 
 summary_preds = data.table(fit$summary_preds)
 summary_preds[, age_group := age_groups[age_index]]
-summary_preds[, date := forecast_date - 49 + time_index]
+summary_preds[, date := forecast_date -  + time_index]
 summary_preds = merge(summary_preds, actualest_inc_long, by=c('date', 'age_group'), all.x = TRUE)
 
-ggplot() +
+ggplot(case_data_age_groups[date>dates[[6]] & date <dates[[6]]+10, ]) +
+  geom_point(aes(x=date, y=rolling_cases, color=age_group))+
   geom_point(data=summary_preds[time_index>=50 & name=='forecast_gens'], aes(x=date, y=`50%`, color=age_group), alpha=0.8)+ 
   geom_ribbon(data=summary_preds[name=='forecast_gens'], aes(x=date, ymin=`5%`, ymax=`95%`, fill=age_group), alpha=0.2)+
   geom_line(data = summary_preds[time_index>20 & time_index<50 & name=='next_gens'], 
-            aes(x=date, y=`50%`, color=age_group), alpha=0.8)+
-  geom_point(data = summary_preds[time_index>50 & name=='forecast_gens'], 
-             aes(x=date, y=value, color=age_group), shape=18, size=3, alpha=0.8)+
-  geom_vline(xintercept = 49)
+            aes(x=date, y=`50%`, color=age_group), alpha=0.8)
--- R/prep_and_run_cases_weekly_example.R
+++ R/prep_and_run_cases_weekly_example.R
@@ -8,6 +8,8 @@ select_age_groups = c("00_04",  "05_09", "10_14",  "15_19", "20_24", "25_29", "3
 
 case_data_ages = case_data[age %in% select_age_groups, ]
 
+
+
 case_data_ages[, lower_age := as.integer(substr(age, 1, 2))]
 case_data_ages[, upper_age := as.integer(substr(age, 4, 5))]
 
@@ -16,8 +18,16 @@ case_data_ages[ is.na(upper_age), upper_age := 90]
 breaks = c(c(0),seq(9,69, 10), c(Inf))
 case_data_ages[, age_group := cut(upper_age, breaks)]
 
+age_groups = unique(cut(1:120, breaks))
 
-case_data_ages[, rolling_cases := sum(rollingSum)/7.0, by = c('age_group', 'date') ]
+select_dates = seq(as.Date(dates[[1]])-14, as.Date(dates[[length(dates)]])+14, 7)
+
+case_data_ages = case_data_ages[ date %in% select_dates, ]
+
+
+case_data_ages[, rolling_cases := sum(rollingSum), by = c('age_group', 'date') ]
+
+case_data_ages
 
 
 case_data_age_groups = unique(case_data_ages[,c('areaCode', 'areaName', 'areaType', 'date', 'age_group', 'rolling_cases')])
@@ -36,12 +46,14 @@ case_matrix_mean[, date := lubridate::ymd(date)]
 cms_cases = readRDS('cms_cases.rds')
 
 age_mod_cases = cmdstan_model('stan/multi-option-contact-model-cases.stan')
-
+period=6*7 # days 
+smax=2 # weeks
 
 fit = fit_NGM_model_for_date_range_cases(
-  end_date = dates[[6]],
+  end_date = dates[[10]],
   age_model = age_mod_cases,
-  period = period+smax, 
+  period = period+smax*7, 
+  smax = smax,
   inf_matrix_mean = case_matrix_mean, 
   inf_matrix_sd = NULL, 
   anb_matrix_mean = NULL, 
@@ -51,21 +63,24 @@ fit = fit_NGM_model_for_date_range_cases(
   
   runindex = 1, 
   quantiles=c(0.05, 0.1, 0.25, 0.5, 0.75, 0.9, 0.95), 
-  contact_option = 1,
-  sigma_option = 1
+  contact_option = 4,
+  sigma_option = 1, 
+  contact_delay = 5, 
+  forecast_horizon = 2
 )
 
 
 summary_preds = data.table(fit$summary_preds)
 summary_preds[, age_group := age_groups[age_index]]
-summary_preds[, date := forecast_date - 49 + time_index]
-summary_preds = merge(summary_preds, actualest_inc_long, by=c('date', 'age_group'), all.x = TRUE)
+summary_preds[, date := forecast_date - period+smax*7 + time_index*7]
+#summary_preds = merge(summary_preds, actualest_inc_long, by=c('date', 'age_group'), all.x = TRUE)
 
-ggplot() +
-  geom_point(data=summary_preds[time_index>=50 & name=='forecast_gens'], aes(x=date, y=`50%`, color=age_group), alpha=0.8)+ 
+
+ggplot()+
+  geom_point(data=summary_preds[time_index>=6 & name=='forecast_gens'], aes(x=date, y=`50%`, color=age_group), alpha=0.8)+ 
   geom_ribbon(data=summary_preds[name=='forecast_gens'], aes(x=date, ymin=`5%`, ymax=`95%`, fill=age_group), alpha=0.2)+
-  geom_line(data = summary_preds[time_index>20 & time_index<50 & name=='next_gens'], 
-            aes(x=date, y=`50%`, color=age_group), alpha=0.8)+
-  geom_point(data = summary_preds[time_index>50 & name=='forecast_gens'], 
-             aes(x=date, y=value, color=age_group), shape=18, size=3, alpha=0.8)+
-  geom_vline(xintercept = 49)
+  geom_line(data = summary_preds[time_index>2 & time_index<6 & name=='next_gens'], 
+            aes(x=date, y=`50%`, color=age_group), alpha=0.8)
+
+  
+summary_preds[name=='forecast_gens']
